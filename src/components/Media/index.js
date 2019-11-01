08b448cd7f4b61f393d9ab864a6ac0d521e141b5
wijayantoap <wijayanto.a.p@gmail.com>
2019-11-01 08:44:48 +0700

    Use normal row col instead of media

--- src/components/Media/index.js
+++ src/components/Media/index.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { Media as MyMedia } from 'react-bootstrap/';
+import { Row, Col } from 'react-bootstrap/';
 import { Link } from "react-router-dom";
 
 const Media = ({ image, title, content, link }) => {
     return(
         <Link to={`/post/${link}`}>
-            <MyMedia>
-                <img
-                width={310}
-                height={175}
-                className="align-self-start mr-3"
-                src={image}
-                alt="Generic placeholder"
-                />
-                <MyMedia.Body>
-                <h5>{title}</h5>
-                <p>
-                    {content}
-                </p>
-                </MyMedia.Body>
-            </MyMedia>
+            <Row className="list_item">
+                <Col md={5}>
+                    <img
+                    width={'100%'}
+                    height={175}
+                    className="align-self-start mr-1"
+                    src={image}
+                    alt="Generic placeholder"
+                    />
+                </Col>
+                <Col md={7}>
+                    <Col>
+                    <h5>{title}</h5>
+                    </Col>
+                    <Col>
+                    <p>{content}</p>
+                    </Col>
+                </Col>
+            </Row>
         </Link>
     );
 };
